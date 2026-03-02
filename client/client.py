544b75bc7fd5fb4e52648c89f28fc67ca592c585
Pradeep Kumar <143413556+yadav-124@users.noreply.github.com>
2026-03-02 07:56:49 +0530

    Made some alignment and corrections in comments to make code more intracting.

--- client/client.py
+++ client/client.py
@@ -1,10 +1,15 @@
-import socket  #It will be used to make network connections using  ipv4(AF_INET) and tcp(SOCK_STREAM)
-import threading #we need it to do more than one task at the same time.
+import socket  
+#It will be used to make network connections using  
+# ipv4(AF_INET) and tcp(SOCK_STREAM)
+import threading 
+#we need it to do more than one task at the same time.
 
-def accept_messages(clientSocket): # a common function to capture the message from server and display it.
+def accept_messages(clientSocket): 
+    # a common function to capture the message from server and display it.
     while True:
         try:
-            mess_recieved=clientSocket.recv(1024).decode() #we will read 1024 bytes at a time and decode them from bytes to readble format
+            mess_recieved=clientSocket.recv(1024).decode() 
+            #we will read 1024 bytes at a time and decode them from bytes to readble format
             
             if not mess_recieved:
                 print("User has Disconnected  himself from server")
@@ -18,16 +23,22 @@ def accept_messages(clientSocket): # a common function to capture the message fr
 
 
 def begin_client():
-    clt=socket.socket(socket.AF_INET,socket.SOCK_STREAM) # we will create a socket  using tcp and ipv4 protocols.
-    clt.connect(("127.0.0.1",5000)) # connect to server that is running on localhost at port 5000
+    clt=socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+    # we will create a socket  using tcp and ipv4 protocols.
+    clt.connect(("127.0.0.1",5000)) 
+    # connect to server that is running on localhost at port 5000
 
     print("Connected to server")
 
-    u_name=input("Please Enter your userName: ") # To recognise different users we ask an username before sending any message
+    u_name=input("Please Enter your userName: ") 
+    # To recognise different users we ask an username before sending any message
 
-    clt.send(f"JOINNING {u_name}".encode()) # we will notify all other clients that this user with username joined the chat
+    clt.send(f"JOINNING {u_name}".encode()) 
+    # we will notify all other clients that this user with username joined the chat
 
-    recv_thread=threading.Thread( # we will make another thread so that the same client can accept other's messages as well as send his message to all others
+    recv_thread=threading.Thread( 
+        # we will make another thread so that the same client can accept 
+        # other's messages as well as send his message to all others
         target=accept_messages,
         args=(clt,)
     )
@@ -35,7 +46,8 @@ def begin_client():
     recv_thread.start()
 
     while True:
-        mess=input("Type a message or type /quit for quitting") # will continue until this client want to quit 
+        mess=input("Type a message or type /quit for quitting") 
+        # will continue until this client want to quit 
 
         if mess=="/quit":
             clt.send("QUITTING".encode())
